857502864e1a175270b90b896c4744dca5f7b037
tom <1042051678@qq.com>
2020-07-10 23:06:32 +0800

    这是将本地java工程提交到本地仓库

---
+++ idea-git-test/src/com/ydxy/domain/User.java
@@ -0,0 +1,11 @@
+package com.ydxy.domain;
+
+/**
+ * @author mrjiang
+ * @date 2020/7/10 22:54
+ **/
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
